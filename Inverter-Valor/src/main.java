5118e284a1d990b65145f6e871691aafd06f8734
Matheus Henrique de Oliveria <62122558+Mthws167@users.noreply.github.com>
2022-03-31 17:58:30 -0300

    Add files via upload

---
+++ Inverter-Valor/src/main.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class main {
+
+
+	public static void main(String[] args) {
+		InverterValor valores = new InverterValor();
+		valores.Lista();
+		
+	}
+
+}
